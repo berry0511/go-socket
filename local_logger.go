6ac681c9c0b476605f8436d2bcff7056b99c32cc
耿沛文 <berry0511@126.com>
2020-09-22 11:36:49 +0800

    change logger format

--- local_logger.go
+++ local_logger.go
@@ -26,7 +26,7 @@ func InitLogger() {
 
 func getEncoder() zapcore.Encoder {
     encoderConfig := zap.NewProductionEncoderConfig()
-    encoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+    encoderConfig.EncodeTime = zapcore.RFC3339NanoTimeEncoder
     encoderConfig.EncodeLevel = zapcore.CapitalLevelEncoder
     return zapcore.NewConsoleEncoder(encoderConfig)
 }
